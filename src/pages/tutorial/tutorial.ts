52e27679ccda64deb278f0158b16922f494cdded
Tobias Schäuble <sbt1@bfh.ch>
2017-12-16 12:45:14 +0100

    task 3.3

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -6,6 +6,8 @@ import { Storage } from '@ionic/storage';
 
 import { TabsPage } from '../tabs-page/tabs-page';
 
+import { Http } from '@angular/http';
+
 @Component({
   selector: 'page-tutorial',
   templateUrl: 'tutorial.html'
@@ -22,13 +24,25 @@ export class TutorialPage {
   constructor(
     public navCtrl: NavController,
     public menu: MenuController,
-    public storage: Storage
+    public storage: Storage,
+    private http: Http
   ) { }
 
   startApp() {
     console.log("username: "+this.username)
     console.log("password: "+this.password)
     console.log("interests: "+this.interests)
+
+    var link = 'proxy/hmad/post.php';
+    var data = ''+this.username+', '+this.password+', '+this.interests;        
+    this.http.post(link, data)
+    .subscribe(data => {
+      console.log("POST RESULT:",data);
+    }, error => {
+      console.log("POST FAILD");
+    });
+
+
     this.navCtrl.push(TabsPage).then(() => {
       this.storage.set('hasSeenTutorial', 'true');
     })
